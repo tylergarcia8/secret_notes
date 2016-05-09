58bfdb0c623c30a948326a57348fd2cc59bd5e37
Tyler Garcia <t-garcia15@hotmail.com>
2016-05-09 12:23:12 -0400

    notes scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509150659) do
+ActiveRecord::Schema.define(version: 20160509162232) do
+
+  create_table "notes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "notes", ["user_id"], name: "index_notes_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
